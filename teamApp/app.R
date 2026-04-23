ed17e80406ceb5625d74f1524807aad2de6feb7d
Lu Liang <luliang@ucdavis.edu>
2026-04-23 16:09:11 -0700

    annotation for ui section

--- teamApp/app.R
+++ teamApp/app.R
@@ -12,20 +12,24 @@ tomato.long <- tomato[c("species", "totleng", "petleng", "leafleng", "leafwid", 
   )
 
 data(tomato.long)
-# Define UI for application 
 
+# Define UI for application 
 ui <- fluidPage( #create the overall page
-    #UI code here
+    #UI code here:
   
-    titlePanel("Tomatooooo"),
+    #Create title for our app
+    titlePanel("Explore Tomato Traits in Different Species:)"),
     
+    #some helpful information for user 
     helpText("This application creates a violin plot to visualize the distribution of the data collected for different traits in different tomato plants."),
     
+    #sidebar setup: 2 types:1. pull down for select species. 2. radiobox to select trait. 
     sidebarLayout(
       sidebarPanel(
-        selectInput("Species","Choose a Species:",
+        #first pull down for species selection
+        selectInput("Species","Choose a Species:", 
                   choices = c("S. pennellii","S. peruvianum","S. chilense","S. chmielewskii","S. habrochaites")),
-      
+        #radio box for trait selection 
         radioButtons("trait","Choose a trait to display:",
                      c("totleng",
                      "petleng",
@@ -33,7 +37,7 @@ ui <- fluidPage( #create the overall page
                      "leafwid",
                      "leafnum")),
     ),
-    
+    #show violin plot for selected species and traits. 
     mainPanel(plotOutput("violinPlot")
       )
     )
