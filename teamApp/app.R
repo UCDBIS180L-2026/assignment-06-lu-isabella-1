58264bed98342c4dda86fb921354e895e99e7461
Lu Liang <luliang@ucdavis.edu>
2026-04-23 15:37:46 -0700

    ui

--- teamApp/app.R
+++ teamApp/app.R
@@ -10,17 +10,36 @@ tomato.long <- tomato[c("species", "totleng", "petleng", "leafleng", "leafwid", 
                names_to = "trait",
                values_to = "number"
   )
-tomato.long
 
+data(tomato.long)
 # Define UI for application 
+
 ui <- fluidPage( #create the overall page
     #UI code here
+  
+    titlePanel("Tomatooooo"),
+    
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("Species","Choose a Species:",
+                  choices = c("S. pennellii","S. peruvianum","S. chilense","S. chmielewskii","S. habrochaites")),
+      
+        radioButtons("trait","Choose a trait to display:",
+                     c("totleng",
+                     "petleng",
+                     "leafleng",
+                     "leafwid ",
+                     "leafnum")),
+    ),
+    
+    mainPanel(plotOutput("violinPlot")
+      )
+    )
   )
-
-
 # Define server logic 
 server <- function(input, output) {
   # server code here
+  
 }
 
 # Run the application 
